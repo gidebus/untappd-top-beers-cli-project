215d7ce3a7fb47483a00f8a6db6e8e1c329003c4
github username <github email address>
2020-07-22 15:52:56 -0500

    add scraper object file

---
+++ lib/untappd_top_beers.rb
@@ -0,0 +1,6 @@
+require "untappd_top_beers/version"
+
+module UntappdTopBeers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/untappd_top_beers/version.rb
@@ -0,0 +1,3 @@
+module UntappdTopBeers
+  VERSION = "0.1.0"
+end
