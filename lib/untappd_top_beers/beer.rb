09f704a83e2f0d6f96f67113cb1e2a668123d7f8
github username <github email address>
2020-07-24 15:36:46 -0500

    new_from_index_page method

--- lib/untappd_top_beers/beer.rb
+++ lib/untappd_top_beers/beer.rb
@@ -4,4 +4,37 @@ class UntappdTopBeers::Beer
 
     @@all = []
 
+    def initialize(name,brewery,style,description,alcohol_content,score,ratings)
+        @name = name
+        @brewery = brewery
+        @style = style
+        @description = description
+        @alcohol_content = alcohol_content
+        @score = score
+        @ratings = ratings
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def find_by_name(name)
+        @@all.detect do |beer|
+            beer.name == name 
+        end
+    end
+
+    def new_from_index_page(beer)
+        self.new(
+            beer.css("div.beer-details p.name").text,
+            beer.css("div.beer-details").css("p")[1].text,
+            beer.css("div.beer-details").css("p")[2].text,
+            beer.css("div.beer-details").css("p")[4].text,
+            beer.css("div.details p.abv").text,
+            beer.css("div.details div.rating-container span.num").text,
+            beer.css("div.details p.raters").text,
+        )
+    end
+
 end
