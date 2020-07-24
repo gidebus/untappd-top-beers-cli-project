9d352403e21df30acf80507e383764743ee5f80b
github username <github email address>
2020-07-24 16:05:10 -0500

    cli print_beer method to display second level of information on beer

--- lib/untappd_top_beers/cli.rb
+++ lib/untappd_top_beers/cli.rb
@@ -4,4 +4,21 @@ class UntappdTopBeers::CLI
         puts "Welcome to Untappd's top 50 beers!"
     end
 
+    def print_beer(beer)
+        puts ""
+        puts "----------- #{beer.name} ------------"
+        puts ""
+        puts "Brewery:           #{beer.brewery}"
+        puts "Style:          #{beer.style}"
+        puts "%ABV:            #{beer.alcohol_content}"
+        puts "Score:             #{beer.score}"
+        puts "Ratings:            #{beer.ratings}"
+    
+        puts ""
+        puts "---------------Description--------------"
+        puts ""
+        puts "#{beer.description}"
+        puts ""
+      end
+
 end
