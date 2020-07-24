957d2145ec7dce9c0f74e16418d6b1cb05ee8993
github username <github email address>
2020-07-24 15:46:56 -0500

    scraper class - method create_beers now calls a beer class method to populate beers

--- lib/untappd_top_beers/Scraper.rb
+++ lib/untappd_top_beers/Scraper.rb
@@ -8,14 +8,14 @@ class UntappdTopBeers::Scraper
         Nokogiri::HTML(open("https://untappd.com/beer/top_rated"))
     end
 
-    def index_page
-        beers = untappd.css("div.beer-container.beer-list.pad div.beer-item")
-        beers.each do |result|
-            puts result.css("p.name").text
-        end
+    def scrape_index_page
+        untappd.css("div.beer-container.beer-list.pad div.beer-item")
     end
 
     def create_beers
+        index_page.each do |beer|
+            UntappdTopBeers::Beer.new_from_index_page(beer)
+        end
     end
 
 end
